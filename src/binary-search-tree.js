ccb79be7faa4d085ef43fee508fc199969484531
aliakseidonut <alexeydomut@gmail.com>
2023-04-17 19:46:56 +0300

    binary-search(root, add, has)

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,41 +6,84 @@ const { Node } = require('../extensions/list-tree.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+
+class Node {
+  constructor(data){
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
 class BinarySearchTree {
 
+  constructor(){
+    this.roodNode = null;
+  }
+
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.roodNode;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    if(this.roodNode == null){
+      this.roodNode = new Node(data);
+    }else{
+      let node = this.roodNode;
+      let newNode = new Node(data);
+      while(node){
+        if(data > node.data){
+          if(!node.right){
+            break;
+          }
+          node = node.right;
+        }else{
+          if(!node.left){
+            break;
+          }
+          node = node.left;
+        }
+      }
+      if(data > node.data){
+        node.right = newNode;
+      }else{
+        node.left = newNode;
+      }
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    let node = this.roodNode;
+    while(node.data != data){
+      if(data > node.data){
+        if(!node.right){
+          return false;
+        }
+        node = node.right;
+      }else{
+        if(!node.left){
+          return false;
+        }
+        node = node.left;
+      }
+    }
+    return true;
   }
 
   find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
   }
 
   remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ 
   }
 }
 
